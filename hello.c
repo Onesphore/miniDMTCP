c808790622ef8217a663f5c453bb9db4e8711711
onesphore <onesphore5@gmail.com>
2018-11-11 22:34:11 -0500

    'sleep()' to 'nanosleep()'

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,12 +11,13 @@
 
 int main(int argc, char *argv[])
 {
+  const struct timespec req = {.tv_sec=1, .tv_nsec=0};
   int i = 0;
   while(1)
   {
     printf("%d.. ", i++);
     fflush(stdout);
-    sleep(1);
+    nanosleep(&req, NULL);
   }
 
 
